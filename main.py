db0cd77146a67e39a1be04d32e0f7cdf2b64da91
aolingwen <747620155@qq.com>
2022-06-13 16:25:59 +0800

    遗漏...

--- main.py
+++ main.py
@@ -11,10 +11,15 @@ from logger import Logger
 if __name__ == "__main__":
     log = Logger(LOG_PATH)
     # 数据加载器
-    dataloader = DataLoader(TEST_IMG_DATA_PATH, log, True)
+    dataloader = DataLoader(TEST_IMG_DATA_PATH, FILTER_MODEL_MAX_BATCH, log, True)
     for i in range(10):
-        dataloader.get_embedding_data_for_query('RIPI202021010600000075')
-    pass
+        print(dataloader.get_embedding_data_for_index())
+
+
+
+
+
+
 
 
 
